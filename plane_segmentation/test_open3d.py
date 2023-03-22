60ccdaf7f3f3f7f9fad1e3a33452f2b73af985f4
Ashwin Vangipuram <ashwinvangipuram@gmail.com>
2023-03-22 11:22:32 -0700

    Working grid generation on reconstructed pointcloud

--- plane_segmentation/test_open3d.py
+++ plane_segmentation/test_open3d.py
@@ -2,46 +2,56 @@ import numpy as np
 import open3d as o3d
 import matplotlib.pyplot as plt
 
-RESOLUTION = 0.025 # meters
+RESOLUTION = .1524 # 6 inches in meters
 
 def view_pc():
     print("Load a ply point cloud, print it, and render it")
     # ply_point_cloud = o3d.data.PLYPointCloud()
     # path = ply_point_cloud.path
-    path = '/storage/pointclouds/ptcloud_0.1grid_colored.ply'
-    # path = '/storage/pointclouds/complete_color_pointcloud.ply'
+    # path = '/storage/pointclouds/ptcloud_0.1grid_colored.ply'
+    path = '/storage/pointclouds/complete_color_pointcloud.ply'
+    # path = "/storage/pointclouds/downsampled_color_pointcloud.ply"
     pcd = o3d.io.read_point_cloud(path)
-    # bbox = o3d.geometry.AxisAlignedBoundingBox([-float('inf'), -float('inf'), -.1], [float('inf'), float('inf'), 3.2])
-    # pcd = pcd.crop(bbox)
-    print(pcd)
+    bbox = o3d.geometry.AxisAlignedBoundingBox([-float('inf'), -float('inf'), -.1], [float('inf'), float('inf'), 2.1])
+    pcd = pcd.crop(bbox)
+    # o3d.io.write_point_cloud(path, pcd)
     # pcd.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamHybrid(radius=0.1, max_nn=30))
+    # print(pcd.normals)
     mesh_frame = o3d.geometry.TriangleMesh.create_coordinate_frame(size=1.0, origin=[0, 0, 0])
-    o3d.visualization.draw_geometries([pcd, mesh_frame],
-                                    zoom=0.3412,
-                                    front=[0.4257, -0.2125, -0.8795],
-                                    lookat=[2.6172, 2.0475, 1.532],
-                                    up=[-0.0694, -0.9768, 0.2024])
+    o3d.visualization.draw_geometries([pcd, mesh_frame])
 
 def plane_seg():
     # pcd_point_cloud = o3d.data.PLYPointCloud()
     # pcd = o3d.io.read_point_cloud(pcd_point_cloud.path)
-    path = '/storage/pointclouds/ptcloud_0.1grid_colored.ply'
+    # path = '/storage/pointclouds/ptcloud_0.1grid_colored.ply'
+    path = '/storage/pointclouds/complete_color_pointcloud.ply'
+    # path = '/storage/pointclouds/downsampled_color_pointcloud.ply'
     pcd = o3d.io.read_point_cloud(path)
-
+    print(pcd)
+    pcd = pcd.voxel_down_sample(voxel_size=0.025)
+    
     outliers = o3d.geometry.PointCloud(pcd)
     print("Total points:", len(outliers.points))
+
+    mesh_frame = o3d.geometry.TriangleMesh.create_coordinate_frame(size=0.5, origin=[0, 0, 0])
+
+    o3d.visualization.draw_geometries([outliers, mesh_frame])
+
+    bbox = o3d.geometry.AxisAlignedBoundingBox([-float('inf'), -float('inf'), -.1], [float('inf'), float('inf'), .1])
+    outliers = outliers.crop(bbox)
+    print("Ground points:", len(outliers.points))
+
+    o3d.visualization.draw_geometries([outliers, mesh_frame])
 
     clouds = []
     planes = []
 
-    mesh_frame = o3d.geometry.TriangleMesh.create_coordinate_frame(size=0.5, origin=[0, 0, 0])
-
     while True:
-        plane_model, inliers = outliers.segment_plane(distance_threshold=0.015,
+        plane_model, inliers = outliers.segment_plane(distance_threshold=0.025,
                                                 ransac_n=10,
                                                 num_iterations=1000)
 
-        if len(inliers) < 7000:
+        if len(inliers) < 10000:
             break
 
         [a, b, c, d] = plane_model
@@ -54,22 +64,12 @@ def plane_seg():
 
         outliers = outliers.select_by_index(inliers, invert=True)
 
-        o3d.visualization.draw_geometries(clouds + [mesh_frame],
-                            zoom=0.8,
-                            front=[-0.4999, -0.1659, -0.8499],
-                            lookat=[2.1813, 2.0619, 2.0999],
-                            up=[0.1204, -0.9852, 0.1215])
-
-    # mesh_sphere = o3d.create_mesh_sphere(radius = 1.0)
-    # mesh_sphere.compute_vertex_normals()
-    # mesh_sphere.paint_uniform_color([0.1, 0.1, 0.7])
-    
-
     print(f"Found {len(clouds)} planes in scene")
+
+    o3d.visualization.draw_geometries(clouds + [mesh_frame])
 
     ground = None
     ground_cloud = None
-    mag = 0
     points = 0
     for i, plane in enumerate(planes):
         z_dir = np.abs(plane[2]) / np.linalg.norm(plane[:3])
@@ -79,6 +79,11 @@ def plane_seg():
             ground_cloud = clouds[i]
     
     print("Ground plane:", ground)
+
+    ground_cloud = o3d.geometry.PointCloud()
+    for c in clouds:
+        ground_cloud += c
+    print("Ground size:", ground_cloud)
     
     ground_points = np.asarray(ground_cloud.points)
     min_bound = np.min(ground_points, axis=0)
@@ -89,42 +94,29 @@ def plane_seg():
     ground_height = np.mean(ground_points[:, 2])
     print("Mean ground height:", ground_height)
 
-    # num_cells = 1000
-    # res = (max_bound - min_bound) / num_cells
-    res = .025
-    num_cells = ((max_bound - min_bound) / res).astype(int) + 1 # Add 1 to include upper limit
+    num_cells = ((max_bound - min_bound) / RESOLUTION).astype(int) + 1 # Add 1 to include upper limit
     print("Num cells:", num_cells)
 
-    grid = np.ones((num_cells[0], num_cells[2]), dtype=np.bool8) # Default is everything is an obstacle
-    grid_min = np.array([min_bound[0], min_bound[2]])
-    grid_max = grid_min + res * np.array(grid.shape)
+    grid = np.ones(num_cells[:2], dtype=np.bool8) # Default is everything is an obstacle
+    grid_min = min_bound[:2]
+    grid_max = grid_min + RESOLUTION * np.array(grid.shape)
     print(grid_min, grid_max)
 
-    # labels = np.array(pcd.cluster_dbscan(eps=0.01, min_points=5, print_progress=True))
-    # max_label = labels.max()
-    # print(f"point cloud has {max_label + 1} clusters")
-    # colors = plt.get_cmap("tab20")(labels / (max_label if max_label > 0 else 1))
-    # colors[labels < 0] = 0
-    # pcd.colors = o3d.utility.Vector3dVector(colors[:, :3])
-    # o3d.visualization.draw_geometries([pcd],
-    #                                 zoom=0.455,
-    #                                 front=[-0.4999, -0.1659, -0.8499],
-    #                                 lookat=[2.1813, 2.0619, 2.0999],
-    #                                 up=[0.1204, -0.9852, 0.1215])
-
-    ground_eps = (max_bound - min_bound)[1]
+    ground_eps = (max_bound - min_bound)[2]
     print("Ground eps:", ground_eps)
 
     dists = pcd.compute_point_cloud_distance(ground_cloud)
     ind = np.where(np.asarray(dists) > 0.001)[0]
     pcd_obstacles = pcd.select_by_index(ind)
 
+    o3d.visualization.draw_geometries([ground_cloud, pcd_obstacles, mesh_frame])
+
     total_points = 0
 
     for i in range(grid.shape[0]):
         for j in range(grid.shape[1]):
-            cell_min = min_bound + [i * res, j * res, -.1]
-            cell_max = min_bound + [(i+1) * res, (j+1) * res, 3]
+            cell_min = min_bound + [i * RESOLUTION, j * RESOLUTION, -.1]
+            cell_max = min_bound + [(i+1) * RESOLUTION, (j+1) * RESOLUTION, 2]
             bbox = o3d.geometry.AxisAlignedBoundingBox(cell_min[:, None], cell_max[:, None])
             # print(bbox)
 
@@ -141,13 +133,11 @@ def plane_seg():
                 if (abs(ground_height - middle[2]) > ground_eps):
                     # Obstacle is here
                     grid[i, j] = 1
-                    # print(f"Obstacle at ({i}, {j})")
 
     print("Totals:")
     print(total_points)
-    print(len(pcd_obstacles.points))
 
-    print(f"Found {np.count_nonzero(grid)} obstacles")
+    print(f"Found {np.count_nonzero(grid)} blocked cells")
     plt.ion()
     plt.imshow(grid.T)
     plt.gca().invert_yaxis()
@@ -155,15 +145,6 @@ def plane_seg():
     plt.show()
     np.save("obstacle_grid.npy", grid)
 
-    o3d.visualization.draw_geometries([ground_cloud, pcd_obstacles, mesh_frame],
-                                    front=[ -0.13628523412698096, -0.99051719584896003, -0.017378713602195017 ],
-                                    lookat=[ 2.6172, 2.0474999999999999, 1.532 ],
-                                    up=[ -0.024578094295810211, -0.014156332132812324, 0.99959767683870282 ],
-                                    zoom=0.78120000000000034)
-                                #   zoom=0.3412,
-                                #   front=[0.4257, -0.2125, -0.8795],
-                                #   lookat=[2.6172, 2.0475, 1.532],
-                                #   up=[-0.0694, -0.9768, 0.2024])
 
 if __name__ == '__main__':
     # plane_seg()
